de9e3397dd5124a90e87936a0abc28e2b9862223
Emma Holden <emmaholden91@yahoo.com>
2025-03-17 22:46:31 +0000

    Created simple search bar component

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+import "./SearchBar.css";
+import { setCurrentCity } from "../../redux/currentCity";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    dispatch(setCurrentCity(inputValue));
+  };
+  return (
+    <div className="search-bar-container">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search for a city"
+        onChange={handleChange}
+      ></input>
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
