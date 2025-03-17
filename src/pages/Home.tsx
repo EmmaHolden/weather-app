8a57686ffd2985752db7b72980bef6e66a6ccf66
Emma Holden <emmaholden91@yahoo.com>
2025-03-17 21:33:29 +0000

    Add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import CurrentWeather from "../components/CurrentWeather/CurrentWeather";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <CurrentWeather />
+    </div>
+  );
+};
+
+export default Home;
