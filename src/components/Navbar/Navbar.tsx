f8539cc8fff70cd7f30891b89df459d103604741
Emma Holden <emmaholden91@yahoo.com>
2025-03-20 00:37:32 +0000

    Remove boilerplate stylesheet and change button for navlink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,24 @@
+import { NavLink } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import "./Navbar.css";
+import { RoutePath } from "../../types/global";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-button-group">
-        <button className="nav-button">Current Weather</button>
-        <button className="nav-button">Detailed Forecast</button>
+        <NavLink
+          to={RoutePath.Home}
+          className={({ isActive }) => (isActive ? "nav-active" : "nav-button")}
+        >
+          Current Weather
+        </NavLink>
+        <NavLink
+          to={RoutePath.ServerError}
+          className={({ isActive }) => (isActive ? "nav-active" : "nav-button")}
+        >
+          Detailed Forecast
+        </NavLink>
       </div>
       <SearchBar />
     </div>
