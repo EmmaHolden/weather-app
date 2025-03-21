31f4c3902cc2508fbd18ba127676ae63b6cd5fdf
Emma Holden <emmaholden91@yahoo.com>
2025-03-21 15:14:03 +0000

    Move weather window class back into parent components to make more flexible

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { getTodayLongDate } from "../../utils/dateUtils";
 import WeatherTempWindow from "../WeatherTempWindow/WeatherTempWindow";
 import "./CurrentWeather.css";
+import classNames from "classnames";
 
 const CurrentWeather = () => {
   const currentDate = getTodayLongDate();
@@ -15,16 +16,26 @@ const CurrentWeather = () => {
     return <div>Loading...</div>;
   }
 
+  const classes = classNames("current-weather-window", {
+    thunderstorm: currentWeather.main === "Thunderstorm",
+    rain: ["Rain", "Drizzle"].includes(currentWeather.main),
+    clear: currentWeather.main === "Clear",
+    snow: currentWeather.main === "Snow",
+    clouds: currentWeather.main === "Clouds",
+    atmosphere: currentWeather.main === "Atmosphere",
+  });
+
   return (
     <div className="current-weather-container main-container">
       <h2 className="no-margin">{currentWeather.cityName}</h2>
       <p>{currentDate}</p>
-      <WeatherTempWindow
-        weatherCategory={currentWeather.main}
-        weatherDescription={currentWeather.description}
-        weatherIcon={currentWeather.icon}
-        weatherTemp={currentWeather.temperature}
-      />
+      <div className={classes}>
+        <WeatherTempWindow
+          weatherDescription={currentWeather.description}
+          weatherIcon={currentWeather.icon}
+          weatherTemp={currentWeather.temperature}
+        />
+      </div>
     </div>
   );
 };
